8931cadf9bd844b9ffa66698ac540f9064a231e8
aasifreact <aasifreact@gmail.com>
2024-11-12 18:52:57 +0530

    all changes done of mail

---
+++ src/content_option.js
@@ -0,0 +1,178 @@
+const logotext = "AASIF";
+const meta = {
+  title: "Shaikh Aasif",
+  description: "I’m Shaikh Aasif UI Developer ,currently working in Pune",
+};
+
+const introdata = {
+  title: "I’m Shaikh Aasif",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "Front-End Developer with 4+ years of experience in web-based user interactions, React development, UI design, JavaScript, and frontend performance optimization. Skilled in translating designs into high-quality code and troubleshooting. Detail-oriented, deadline-driven, and experienced in delivering robust web interfaces",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "about my self",
+  aboutme:
+    "Front-End Developer with 4+ years of experience in webbased user interactions, React development, UI design, JavaScript, and frontend performance optimization. Skilled in translating designs into high-quality code and troubleshooting. Detail-oriented, deadline-driven, and experienced in delivering robust web interfaces",
+};
+const worktimeline = [
+  {
+    jobtitle: "software Engineer",
+    where: "TextCode Technologies",
+    date: "2020",
+  },
+  {
+    jobtitle: "Ui developer",
+    where: "Anzu Technologies",
+    date: "2023",
+  },
+];
+
+const skills = [
+  {
+    name: "HTML5",
+    value: 95,
+  },
+  {
+    name: "Css3",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 85,
+  },
+  {
+    name: "redux",
+    value: 55,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description: "good at design in both UI & UX",
+  },
+  {
+    title: "Mobile Apps",
+    description: "mobile friendly sites with the help of css and mediaquery",
+  },
+  {
+    title: "React.js",
+    description:
+      "Single page application using most famouse javascript library React.js",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "aasifreact@gmail.com",
+  YOUR_FONE: "+91 8888210441",
+  description: "lets work togther",
+  YOUR_SERVICE_ID: "service_pq9jaiw",
+  YOUR_TEMPLATE_ID: "template_o7870je",
+  YOUR_USER_ID: "vuaJvmJ9hP4h4u6NL",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
